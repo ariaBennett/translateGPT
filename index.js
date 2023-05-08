cf886a3c57f39b8f62b8e4364e5feabfb70eb189
ariabennett <twoplustwo@gmail.com>
2023-05-08 13:50:03 +0100

    add dotenv

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-export const translate = (source) => {
+require("dotenv").config();
+
+const translate = (source) => {
+  const apiKey = process.env.OPENAI_API_KEY;
+  if (!apiKey) {
+    throw new Error("Please set the OPENAI_API_KEY environment variable");
+  }
   console.log(source);
+  console.log(apiKey);
+};
+
+translate({ hi: "meow" });
+
+module.exports = {
+  translate: translate,
 };
