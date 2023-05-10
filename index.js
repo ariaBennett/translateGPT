a4e0e2f3906cb13f2423d27f8ad68f9304d0c2fc
ariabennett <twoplustwo@gmail.com>
2023-05-10 13:11:13 +0100

    Build queries new JSON way

--- index.js
+++ index.js
@@ -13,7 +13,88 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const isObject = function (a) {
+  return !!a && a.constructor === Object;
+};
+
+const isArray = function (a) {
+  return !!a && a.constructor === Array;
+};
+
+// TODO Json smart restructure
+// Get source data
+
+// While loop on object that builds queries out of blank values
+//   and token based splits them
 async function translate(input) {
+  // Turn source into JSON object with blank values
+  const formatInput = (input) => {
+    const formattedInput = {};
+
+    if (isObject(input)) {
+      Object.keys(input).forEach((key) => {
+        formattedInput[key] = "";
+      });
+    } else if (isArray(input)) {
+      input.forEach((key) => {
+        formattedInput[key] = "";
+      });
+    } else {
+      console.log("Input must either be an object or an array.");
+      return null;
+    }
+
+    return formattedInput;
+  };
+
+  const buildQueries = (formattedInput) => {
+    const tokenLimit = 1000;
+    const queries = [];
+    let buildingTokens = 0;
+    let buildingQuery = {};
+
+    const getTokenCount = (str) => {
+      return encode(str).length;
+    };
+
+    Object.keys(formattedInput).forEach((key) => {
+      if (buildingTokens >= tokenLimit) {
+        queries.push(JSON.stringify(buildingQuery));
+        buildingTokens = 0;
+        buildingQuery = {};
+      }
+
+      if (formattedInput[key] === "") {
+        buildingQuery[key] = "";
+        buildingTokens += getTokenCount(key) * 2 + 4;
+      }
+    });
+
+    if (buildingQuery) {
+      queries.push(JSON.stringify(buildingQuery));
+    }
+
+    return queries;
+  };
+
+  const formattedInput = formatInput(input);
+  console.log("builtQueries", buildQueries(formattedInput));
+}
+// Try function on result from api to place the values into source json
+// once while loop completes, return filled JSON
+
+(async () => {
+  const result = await translate(input);
+  // console.log("result", result);
+  // console.log(result);
+  // const encoded = encode(JSON.stringify(input));
+  // for (let token of encoded) {
+  //   console.log({ token, string: decode([token]) });
+  // }
+  // console.log(chunkInput(input));
+})();
+
+async function translateOld(input) {
   if (!configuration.apiKey) {
     console.log(
       "OpenAI API key not configured, please follow instructions in README.md"
@@ -77,10 +158,6 @@ const queryTranslation = async (chunk) => {
   }
 };
 
-const getTokenCount = (str) => {
-  return encode(str).length;
-};
-
 const chunkKeys = (keys) => {
   const tokensPerChunk = 1000;
   let chunks = [];
@@ -113,8 +190,8 @@ module.exports = {
 };
 
 (async () => {
-  const result = await translate(input);
-  console.log("result", result);
+  // const result = await translate(input);
+  // console.log("result", result);
   // console.log(result);
   // const encoded = encode(JSON.stringify(input));
   // for (let token of encoded) {
